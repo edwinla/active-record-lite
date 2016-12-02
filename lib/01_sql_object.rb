8e3c43d56c1ab431e9aba4aca355c3d8ac7ed1d3
Edwin La <edwin@Edwins-MacBook-Pro.local>
2016-12-02 14:22:42 -0800

    Add pre-finalize class methods

--- lib/01_sql_object.rb
+++ lib/01_sql_object.rb
@@ -5,17 +5,29 @@ require 'active_support/inflector'
 
 class SQLObject
   def self.columns
-    columns = DBConnection.execute2(<<-SQL)
+    return @columns if @columns
+    cols = DBConnection.execute2(<<-SQL).first
       SELECT
         *
       FROM
-        #{table_name}
+        #{self.table_name}
+      LIMIT
+        0
     SQL
 
-    columns[0].map(&:to_sym)
+    @columns = cols.map!(&:to_sym)
   end
 
   def self.finalize!
+    self.columns.each do |col|
+      define_method("#{col}=") do |val|
+        self.attributes[col] = val
+      end
+
+      define_method(col) do
+        self.attributes[col]
+      end
+    end
   end
 
   def self.table_name=(table_name)
@@ -27,35 +39,64 @@ class SQLObject
   end
 
   def self.generate_table_name
-    ActiveSupport::Inflector.pluralize(self.name).downcase
+    ActiveSupport::Inflector.pluralize(self.name.underscore).downcase
   end
 
   def self.all
-    # ...
+    results = DBConnection.execute(<<-SQL)
+      SELECT
+        *
+      FROM
+        #{self.table_name}
+    SQL
+
+    self.parse_all(results)
   end
 
   def self.parse_all(results)
-    # ...
+    results.map { |result| self.new(result) }
   end
 
   def self.find(id)
-    # ...
+    result = DBConnection.execute(<<-SQL, id).first
+      SELECT
+        *
+      FROM
+        #{self.table_name}
+      WHERE
+        id = ?
+    SQL
+
+    result ? self.new(result) : nil
   end
 
   def initialize(params = {})
-    # ...
+    params.each do |attr_name, value|
+      attr_name = attr_name.to_sym
+      raise "unknown attribute '#{attr_name}'" unless self.class.columns.include?(attr_name)
+      self.send("#{attr_name}=", value)
+    end
   end
 
   def attributes
-    # ...
+    @attributes ||= {}
   end
 
   def attribute_values
-    # ...
+    self.class.columns.map { |col| self.send(col) }
   end
 
   def insert
-    # ...
+    col_name_ary = self.class.columns.join(",")
+    question_marks = ["?"] * col_name_ary.count
+
+    DBConnection.execute(<<-SQL, *attribute_values)
+      INSERT INTO
+        #{self.table_name} col_name_ary
+      VALUES
+        question_marks
+    SQL
+
   end
 
   def update
