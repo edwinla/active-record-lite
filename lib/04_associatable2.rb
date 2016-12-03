c93f507890e1c314ef83b0c7913a0b08b7ad7ac9
Edwin La <edwin@edwins-mbp.attlocal.net>
2016-12-02 22:51:21 -0800

    Add has_one_through method to module

--- lib/04_associatable2.rb
+++ lib/04_associatable2.rb
@@ -5,6 +5,21 @@ module Associatable
   # Remember to go back to 04_associatable to write ::assoc_options
 
   def has_one_through(name, through_name, source_name)
-    # ...
+
+
+    define_method(name) do
+
+      through_options = self.class.assoc_options[through_name]
+      source_options = through_options.model_class.assoc_options[source_name]
+
+      tt = through_options.table_name
+      st = source_options.table_name
+      t_pk = through_options.primary_key
+      s_pk = source_options.primary_key
+      t_fk = through_options.foreign_key
+      s_fk = source_options.foreign_key
+      t_fk_val = self.send(t_fk)
+
+    end
   end
 end
